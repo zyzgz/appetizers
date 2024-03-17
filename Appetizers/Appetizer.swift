5106cafac9f168213deb55343adfc7949614d766
Mateusz <mateusz.obloza.0@gmail.com>
2024-03-17 16:13:28 +0100

    feat: add appetizers model

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Mateusz Obłoza on 17/03/2024.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for my Appetizer. It's yummy!",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
