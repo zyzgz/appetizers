348453cedfb9ab209a37127474d85e263478c1d8
Mateusz <mateusz.obloza.0@gmail.com>
2024-03-17 15:09:22 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Mateusz Obłoza on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by Mateusz Obłoza on 17/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
