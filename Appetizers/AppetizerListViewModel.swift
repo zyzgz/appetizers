6b62e915fbc7e1a1459eab7f6f55676643bcb2e7
Mateusz <mateusz.obloza.0@gmail.com>
2024-03-23 09:28:23 +0100

    feat: errors & alerts

--- Appetizers/AppetizerListViewModel.swift
+++ Appetizers/AppetizerListViewModel.swift
@@ -10,16 +10,29 @@ import SwiftUI
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
         NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [self] in
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
-                case .failure(let error):
-                    print(error.localizedDescription)
                     
+                case .failure(let error):
+                    switch error {
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                        
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                        
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                        
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
