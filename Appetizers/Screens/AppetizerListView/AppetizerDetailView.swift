c40eb3de7d799fc4ec30f249c1a8e610c3223644
Mateusz <mateusz.obloza.0@gmail.com>
2024-03-24 16:48:25 +0100

    feat: detail view UI connections

--- Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct AppetizerDetailView: View {
     
     let appetizer: Appetizer
+    @Binding var isShowingDetail: Bool
     
     var body: some View {
         VStack {
-            Image("asian-flank-steak")
-                .resizable()
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 300, height: 225)
             
@@ -84,7 +84,7 @@ struct AppetizerDetailView: View {
         .cornerRadius(12)
         .shadow(radius: 40)
         .overlay(Button {
-            print("dismiss")
+            isShowingDetail = false
         } label: {
             ZStack {
                 Circle()
@@ -102,5 +102,6 @@ struct AppetizerDetailView: View {
 }
 
 #Preview {
-    AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+    AppetizerDetailView(appetizer: MockData.sampleAppetizer, 
+                        isShowingDetail: .constant(true))
 }
--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -10,17 +10,30 @@ import SwiftUI
 struct AppetizerListView: View {
     
     @StateObject var viewModel = AppetizerListViewModel()
+    @State private var isShowingDetail = false
+    @State private var selectedAppetizer: Appetizer?
     
     var body: some View {
         ZStack {
             NavigationView {
                 List(viewModel.appetizers, id: \.id) { appetizer in
                     AppetizerListCell(appetizer: appetizer)
+                        .onTapGesture {
+                            isShowingDetail = true
+                            selectedAppetizer = appetizer
+                        }
                 }
                 .navigationTitle("🍕 Appetizers")
+                .disabled(isShowingDetail)
             }
             .onAppear() {
                 viewModel.getAppetizers()
+            }
+            .blur(radius: isShowingDetail ? 20 : 0)
+            
+            if isShowingDetail {
+                AppetizerDetailView(appetizer: selectedAppetizer!,
+                                    isShowingDetail: $isShowingDetail)
             }
             
             if viewModel.isLoading {
