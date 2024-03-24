555ca7884a9b90168880b1b2fb742114a7a25c68
Mateusz <mateusz.obloza.0@gmail.com>
2024-03-24 09:40:33 +0100

    feat: download image

--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -5,10 +5,12 @@
 //  Created by Mateusz Obłoza on 18/03/2024.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
+    
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
@@ -44,6 +46,34 @@ final class NetworkManager {
             } catch {
                 completed(.failure(.invalidData))
             }
+        }
+        
+        task.resume()
+    }
+    
+    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, response, error in
+         
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
         }
         
         task.resume()
