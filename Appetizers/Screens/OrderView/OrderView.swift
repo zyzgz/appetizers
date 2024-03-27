6d7a047db00810d3828605312b1c20500070cb58
Mateusz <mateusz.obloza.0@gmail.com>
2024-03-27 18:34:32 +0100

    feat: empty state

--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -13,23 +13,31 @@ struct OrderView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    ForEach(orderItems) { appetizer in
-                        AppetizerListCell(appetizer: appetizer)
+            ZStack {
+                VStack {
+                    List {
+                        ForEach(orderItems) { appetizer in
+                            AppetizerListCell(appetizer: appetizer)
+                        }
+                        .onDelete(perform: deleteItems)
                     }
-                    .onDelete(perform: deleteItems)
+                    .listStyle(PlainListStyle())
+                    
+                    Button {
+                        print("Order Placed")
+                    } label: {
+                        APButton(title: "$99.99 - Place Order")
+                    }
+                    .padding(.bottom, 25)
                 }
-                .listStyle(PlainListStyle())
                 
-                Button {
-                    print("Order Placed")
-                } label: {
-                    APButton(title: "$99.99 - Place Order")
+                if orderItems.isEmpty {
+                    EmptyState(imageName: "empty-order",
+                               message: "You have no items in your order. \nPlease add an appetizer!")
                 }
-                .padding(.bottom, 25)
             }
             .navigationTitle("🧾 Order")
+            
         }
     }
     
---
+++ Appetizers/Views/Empty States/EmptyState.swift
@@ -0,0 +1,40 @@
+//
+//  EmptyState.swift
+//  Appetizers
+//
+//  Created by Mateusz Obłoza on 27/03/2024.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    
+    let imageName: String
+    let message: String
+    
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                
+                Text(message)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .padding()
+            }
+            .offset(y: -40)
+        }
+    }
+}
+
+#Preview {
+    EmptyState(imageName: "empty-order", message: "This is test message.")
+}
